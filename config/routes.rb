6886d4a28d12dd49a881746bd521de0678aa3588
Jackrim1 <jriminton@gmail.com>
2017-01-31 14:54:39 +0000

    3/4 way through editing custom forms for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  resources :projects do
+    resources :tasks, except: [:index], controller: 'projects/tasks'
+  end
+
+  root "projects#index"
+
 end
